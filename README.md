40b37622394238564961eae85eaa3b311dc63a8b
Andrew Tran <andrewdaotran@gmail.com>
2023-03-30 11:27:28 -0700

    Issue with train.py line 69 train_loader

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 Install Pytorch and dependencies
 
+`pip3 install torch torchvision`
+
+Can test if PyTorch is installed if test.py runs successfully
+
 Follow documentation to install PyTorch
 
 `pip install nltk`
+
+In `nltk_utils.py` make sure to `nltk.download('punkt')`
--- train.py
+++ train.py
@@ -2,6 +2,12 @@ import json
 from nltk_utils import tokenize, stem, bag_of_words
 import numpy as np
 
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+from model import NeuralNet
+
 with open('intents.json', 'r') as f:
     intents = json.load(f)
 
@@ -36,3 +42,57 @@ for (pattern_sentence, tag) in xy:
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+    
+    def __len___(self):
+        return self.n_samples
+
+# Hyperparameters
+batch_size = 8
+hidden_size = 8
+output_size = len(tags)
+input_size = len(X_train[0]) # Same size as len(all_words), what we used is the first bag of words and each bag of words is the size of all_words
+learning_rate = 0.001
+num_epochs = 1000
+
+
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2) # Having issue with this for some reason
+
+# Device agnostic
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+# Loss and Optimizer
+loss_fn = nn.CrossEntropyLoss() # Or criterion
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Training loop
+for epoch in range(num_epochs):
+    for(words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device)
+
+        # forward pass
+        outputs = model(words)
+        loss = loss_fn(outputs, labels)
+
+        # backward and optimizer step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch + 1 % 100 == 0):
+        print(f'epoch {epoch+1}/{num_epochs}, loss={loss.item():.4f}')
+
+print(f'final loss, loss={loss.item():.4f}')
